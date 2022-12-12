02259eb34dfd0481b2867f2970f1c149155bdf20
IzzyKhalifa <izzy.e.khalifa@gmail.com>
2022-12-12 20:53:38 +1100

    bugs fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
 const { User } = require('../models')
 
 const userController = {
-    //get all users
     getAllUsers(req,res) {
         User.find({})
         .populate({
@@ -16,7 +15,6 @@ const userController = {
         });
       },
     
-    //get User by ID with thoughts
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
            .populate({
@@ -35,14 +33,12 @@ const userController = {
         });
      },
 
-     //create User
      createUser({ body }, res) {
          User.create(body)
          .then(dbUserData => res.json(dbUserData))
          .catch(err => res.status(400).json(err));
      },
 
-     //add friend
      addFriend({ params }, res) {
          User.findOneAndUpdate(
              {_id: params.userId},
@@ -59,7 +55,6 @@ const userController = {
          .catch(err => res.json(err));
      },
 
-     //update User
      updateUser({ params, body}, res) {
          User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true})
          .then(dbUserData => {
@@ -72,7 +67,6 @@ const userController = {
             .catch(err => res.json(err))
      },
 
-     //delete User
      deleteUser({ params }, res) {
          User.findOneAndDelete({ _id: params.id })
          .then(dbUserData => {
@@ -85,7 +79,6 @@ const userController = {
          .catch(err => res.status(400).json(err))
      },
 
-     //remove Friend
      removeFriend( { params }, res) {
          User.findOneAndUpdate(
              { _id: params.userId },
